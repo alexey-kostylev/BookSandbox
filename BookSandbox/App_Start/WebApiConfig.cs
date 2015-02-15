74e3a5558df9ddb972377718a7081a39c951ad5e
amadonus <alx.kost@gmail.com>
2015-02-15 21:10:37 +1300

    routing

--- BookSandbox/App_Start/WebApiConfig.cs
+++ BookSandbox/App_Start/WebApiConfig.cs
@@ -20,6 +20,58 @@ namespace BookSandbox
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //config.Routes.MapHttpRoute(
+            //    name: "Details",
+            //    routeTemplate: "api/My/{id}",
+            //    defaults: new { id = RouteParameter.Optional, action = "GetDetails" },
+            //    constraints: new { id = @"\d*" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "Echo",
+            //    routeTemplate: "api/My/{str}",
+            //    defaults: new { action = "GetEcho" }
+            // ); 
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DetailsEx",
+            //    routeTemplate: "api/My/{category}/{year}",
+            //    defaults: new { action = "GetDetailsEx", controller = "My" },
+            //    constraints: new { year = @"\d*" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //   name: "DefaultMyApi1",
+            //   routeTemplate: "api/My/{id}",
+            //   defaults: new { id = RouteParameter.Optional, controller = "My" },
+            //   constraints: new { id = @"\d*" }
+            //); 
+
+            //config.Routes.MapHttpRoute(
+            //   name: "DefaultMyApi",
+            //   routeTemplate: "api/My/{str}",
+            //   defaults: new { str = RouteParameter.Optional, controller="My" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "My",
+            //    routeTemplate: "api/My/{echo}",
+            //    defaults: new { echo = RouteParameter.Optional, controller="My" }                
+            //);
+
+            config.Routes.MapHttpRoute(
+                name: "My",
+                routeTemplate: "api/My/{id}",
+                defaults: new { echo = RouteParameter.Optional, controller="My" },
+                constraints: new { id=@"\d+" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "MyEcho",
+                routeTemplate: "api/{controller}/{echo}",
+                defaults: new { echo = RouteParameter.Optional }
+            );
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ BookSandbox/Controllers/MyController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookSandbox.Controllers
+{
+    public class MyController : ApiController
+    {
+        public string GetDefaultEcho()
+        {
+            return string.Format("GetDefaultEcho: hello from 'default'");
+        }
+
+        public string GetEcho(string echo)
+        {
+            return string.Format("GetEcho: hello from '{0}'", echo ?? "null");
+        }
+
+        //public string GetSayHello()
+        //{
+        //    return "GetSayHello: Hi there!";
+        //}
+
+        public string GetDetails(int id)
+        {
+            return string.Format("GetDetails: details for id='{0}'", id);
+        }
+        
+        //public string GetEcho(string str)
+        //{
+        //    return string.Format("GetEcho: hello from '{0}'", str);
+        //}
+
+        //public string GetDetailsEx(string category, int year)
+        //{
+        //    return string.Format("GetDetailsEx: details for ['{0}' - {1}]", category, year);
+        //}
+    }
+}
